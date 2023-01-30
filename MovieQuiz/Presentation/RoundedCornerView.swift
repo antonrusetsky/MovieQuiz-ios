937abd6b191c004178f9abd8c03ed3f9cff8649c
Anton Rusetsky <123160771+antonrusetsky@users.noreply.github.com>
2023-01-30 20:17:55 +0700

    Задача по спринту 4

---
+++ MovieQuiz/Presentation/RoundedCornerView.swift
@@ -0,0 +1,21 @@
+//
+//  RoundedCornerView.swift
+//  MovieQuiz
+//
+//  Created by wfz on 27.01.2023.
+//
+
+import UIKit
+
+@IBDesignable extension UIView {
+    @IBInspectable var cornerRadius: CGFloat {
+        get { return layer.cornerRadius }
+        set {
+              layer.cornerRadius = newValue
+
+              // If masksToBounds is true, subviews will be
+              // clipped to the rounded corners.
+              layer.masksToBounds = (newValue > 0)
+        }
+    }
+}
